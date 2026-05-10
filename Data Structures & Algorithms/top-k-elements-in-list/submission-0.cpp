e744332badd7ff4cbe4cc0870097ee8b3a2f8ac7
Vaidehi Zade <60578475+Vaidehizade@users.noreply.github.com>
2026-05-10 15:43:46 +0530

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int,int> mp;
+        vector<int>ans;
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]++;
+        }
+        while(k--){
+            int maxi = 0;
+            int element;
+            for(auto x: mp){
+                if(x.second > maxi){
+                    maxi = x.second;
+                    element = x.first;
+                }
+            }
+            ans.push_back(element);
+            mp.erase(element);
+        }
+        return ans;
+
+        //TC: O(n log n + k*n)
+    }
+};
