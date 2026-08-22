077544c49c82a26ce3bacca65fadd4781d992036
Vaidehi Zade <60578475+Vaidehizade@users.noreply.github.com>
2026-08-22 11:45:43 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+    //     if(s.size() != t.size()){
+    //         return false;
+    //     }
+    //     map<char,int>mp;
+    //     for(char c: s){
+    //         mp[c]++;
+    //     }
+    //     for(char c: t){
+    //         mp[c]--;
+    //     }
+
+    //    for(auto x:mp){
+    //       if(x.second !=0){
+    //          return false;
+    //       }
+    //    } 
+    //    return true;   
+
+       //TC: O(n);
+       //SC: O(n);
+
+
+    if(s.size() != t.size()) return false;
+    int freq[26] = {0};
+
+    for(char c: s){
+        freq[ c - 'a']++;
+    }
+
+    for(char c: t){
+        freq[ c - 'a']--;
+    }
+
+    for(int i=0; i<26; i++){
+        if(freq[i]!=0)
+        return false;
+    }
+    return true;
+
+    }
+
+    //TC: O(N)
+    //SC: O(1)
+};
