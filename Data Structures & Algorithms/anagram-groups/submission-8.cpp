8167d7bfd4d198b750a70a24f404f57b839dead5
Vaidehi Zade <60578475+Vaidehizade@users.noreply.github.com>
2026-08-30 18:08:02 +0530

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        //frequency couting + unordered map
+        unordered_map<string, vector<string>>mp;
+        for(string str : strs){
+            //frequency of 26 lowercase letters
+            vector<int> count(26,0);
+            for(char c: str){
+                count[c - 'a']++;
+            }
+            //Convert frequency array into a string key
+            string key;
+            for(int i=0; i < 26; i++){
+                key += to_string(count[i]) + "#";
+            }
+            mp[key].push_back(str);
+        }
+
+        vector<vector<string>> res;
+
+        for(auto x: mp){
+            res.push_back(x.second);
+        }
+        return res;
+
+        //optimal solution 
+        //where N = number of strings and K = maximum string length.
+        //TC: O(N*K);
+        //SC: O(N*K);
+
+    }
+};
