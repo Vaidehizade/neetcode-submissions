a9a62fd821389ad7ff298a2e5365915b380ee771
Vaidehi Zade <60578475+Vaidehizade@users.noreply.github.com>
2026-08-30 17:30:28 +0530

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String,List<String>> mp = new HashMap<>();
+
+        for(String str: strs){
+
+            //convert string to character array
+            char[] chars = str.toCharArray();
+            //sort characters
+            Arrays.sort(chars);
+
+            //sorted string becomes the key
+            String key = new String(chars);
+
+            //add original string
+            //mp.computeIfAbsent(key, k-> new ArrayList<>()).add(str);
+            if(!mp.containsKey(key)){
+                mp.put(key, new ArrayList<>());
+            }
+            mp.get(key).add(str);
+
+        }
+        return new ArrayList<>(mp.values());
+    }
+}
