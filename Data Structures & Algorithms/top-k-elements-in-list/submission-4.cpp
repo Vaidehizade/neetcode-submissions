b9372384f090ecb681d23026dbdf2cd1b3c72219
Vaidehi Zade <60578475+Vaidehizade@users.noreply.github.com>
2026-09-08 23:04:20 +0530

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int,int>mp;
+        for(int i=0; i<nums.size(); i++){ //O(n)
+           mp[nums[i]]++;
+        }
+        // 2. Store {frequency, number}
+        vector<pair<int, int>> freq;
+        for (auto x : mp) {
+            freq.push_back({x.second, x.first}); //O(n)
+        }
+        // 3. Sort by frequency
+        sort(freq.rbegin(), freq.rend()); //O(nlog n)
+        // 4. Take first k elements
+        vector<int> ans;
+        for (int i = 0; i < k; i++) {
+            ans.push_back(freq[i].second);
+        }
+    //TC: O(2*n) + O(nlogn)
+        return ans;
+    }
+};
